f9bc5db6064445743d877818a8f08325f6d10070
Remon <remongerges94@gmail.com>
2019-07-06 09:28:00 +0200

    Handle click events for fragment items

--- app/src/main/java/com/example/tempus/MainActivity.java
+++ app/src/main/java/com/example/tempus/MainActivity.java
@@ -1,13 +1,18 @@
 package com.example.tempus;
 
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity
+        implements NavigationView.OnNavigationItemSelectedListener {
 
     private DrawerLayout drawer;
 
@@ -21,12 +26,50 @@ public class MainActivity extends AppCompatActivity {
 
         drawer = findViewById(R.id.drawer_layout);
 
+        NavigationView navigationView = findViewById(R.id.nav_view);
+
+        navigationView.setNavigationItemSelectedListener(this);
+
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer, toolbar,
                 R.string.navigation_drawer_open, R.string.navigation_drawer_close);
 
         drawer.addDrawerListener(toggle);
 
         toggle.syncState();
+
+        if (savedInstanceState == null) {
+            //When we start the activity we open the first item imediately
+            getSupportFragmentManager().beginTransaction()
+                    .replace(R.id.fragment_container, new ProjectFragment()).commit();
+
+            navigationView.setCheckedItem(R.id.nav_projects);
+        }
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+        switch (menuItem.getItemId()) {
+            case R.id.nav_projects:
+                getSupportFragmentManager().beginTransaction()
+                        .replace(R.id.fragment_container, new ProjectFragment()).commit();
+                break;
+
+            case R.id.nav_tasks:
+                getSupportFragmentManager().beginTransaction()
+                        .replace(R.id.fragment_container, new TaskFragment()).commit();
+                break;
+
+            case R.id.nav_notes:
+                getSupportFragmentManager().beginTransaction()
+                        .replace(R.id.fragment_container, new NoteFragment()).commit();
+                break;
+
+            case R.id.nav_contact:
+                Toast.makeText(this, "Contact us at...", Toast.LENGTH_SHORT).show();
+                break;
+        }
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
     }
 
     @Override
